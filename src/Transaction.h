db9daf64cb9da1c91d9f56eb9f8024afc619e6e5
Calebf2424 <97080069+Calebf2424@users.noreply.github.com>
2024-06-28 13:30:22 -0600

    Update Transaction.h

--- src/Transaction.h
+++ src/Transaction.h
@@ -3,24 +3,19 @@
 
 #include <string>
 #include <vector>
-#include <ctime>
 
 class Transaction {
  public:
-
   //user inputted constructor
   Transaction();
 
-    double getAmount() const { return amount; }
-    std::string getDesc() const { return desc; }
-    std::time_t getDate() const { return date; }
+  double getAmount() const { return amount; }
+  std::string getDesc() const { return desc; }
 
  private:
   double amount;
   std::string desc;
-  std::time_t date;
   void userInput();
-  std::time_t userInputDate();
 };
 
 
