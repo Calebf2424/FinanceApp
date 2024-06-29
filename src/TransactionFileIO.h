0533aa65c736ad0a6490a490213907e229e034a2
Calebf2424 <97080069+Calebf2424@users.noreply.github.com>
2024-06-29 16:04:14 -0600

    Update TransactionFileIO.h

--- src/TransactionFileIO.h
+++ src/TransactionFileIO.h
@@ -7,8 +7,8 @@
 
 class TransactionFileIO {
  public:
-  static void saveToFile(const std::vector<Transaction>& transactions, const std::string& filename);
-  static std::vector<Transaction> loadFromFile(const std::string& filename);
+  static void saveToFile(const std::vector<Transaction*>& transactions, const std::string& filename);
+  static std::vector<Transaction*> loadFromFile(const std::string& filename);
 
 };
 #endif
