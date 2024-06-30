2cc79d176b7bd3ab291311dfbb3e67dc5945f387
Calebf2424 <cfarnsworth2424@gmail.com>
2024-06-30 17:02:40 -0600

    updated Utility

--- src/Utility.cpp
+++ src/Utility.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <limits>
+#include <ctime>
 #include "Utility.h"
 
 bool isValidInt(int &input, int min, int max) {
@@ -11,4 +12,19 @@ bool isValidInt(int &input, int min, int max) {
         return false;
     }
     return true;
+}
+
+int getDaysInMonth(int month, int year) {
+    if (month == 2) {
+        // Check if the year is a leap year
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            return 29;
+        } else {
+            return 28;
+        }
+    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        return 30;
+    } else {
+        return 31;
+    }
 }
--- src/Utility.h
+++ src/Utility.h
@@ -2,5 +2,6 @@
 #define UTILS_H
 
 bool isValidInt(int &input, int min, int max);
+int getDaysInMonth(int month, int year);
 
 #endif // UTILS_H
