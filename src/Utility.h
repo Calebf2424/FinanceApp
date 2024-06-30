4c32839c584564f25133b5e5868e0638fd0ca595
Calebf2424 <97080069+Calebf2424@users.noreply.github.com>
2024-06-30 11:22:15 -0600

    Update Utility.h

--- src/Utility.h
+++ src/Utility.h
@@ -1,7 +1,13 @@
-// utils.h
 #ifndef UTILS_H
 #define UTILS_H
 
 bool isValidInt(int &input, int min, int max);
+bool isValidDouble(const std::string& str) {
+    std::istringstream iss(str);
+    double d;
+    char c;
+    // Try to parse the string as a double and ensure there are no extra characters
+    return iss >> d && !(iss >> c);
+}
 
 #endif // UTILS_H
