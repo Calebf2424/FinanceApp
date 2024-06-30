8c54a62b89576e8930e145c7209cfe671dbcd55b
Calebf2424 <97080069+Calebf2424@users.noreply.github.com>
2024-06-29 22:12:25 -0600

    Update TransactionManager.h

--- src/TransactionManager.h
+++ src/TransactionManager.h
@@ -13,6 +13,7 @@ public:
     double getSum();
     double getAccountBalance() const;
     double setAccountBalance();
+    std::vector<Transaction>& getAllTransactions() const;
 
 private:
     std::vector<Transaction> transactions;
